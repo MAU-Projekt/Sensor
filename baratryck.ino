505d0b386b03da6f62913dd15ffb1d1ea2f32760
alexhultman-2fa <58995996+alexhultman-2fa@users.noreply.github.com>
2021-04-27 20:26:43 +0200

    Ändra path

--- baratryck.ino
+++ baratryck.ino
@@ -10,7 +10,7 @@ dht DHT;
   int ledpin = 11;
 
 const char kHostname[] = "84.217.9.249";
-const char kPath[] = "/sensor/ar/now";
+const char kPath[] = "/data/ar";
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
